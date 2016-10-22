4773a1f64c743ec20882d1548a305a169c9c65d7
Cody Pettit (Smaug) <cpett26@gmail.com>
2016-10-22 16:01:23 -0600

    Adds general design and Adds test Tableau chart

---
+++ ihc_data/ihc/static/js/tableau.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  /////////////////////
+  // Global variables
+  var viz, workbook, activeSheet
+
+  // Reload the viz
+  $('#resetViz').click(function(){
+    console.log('here')
+    initViz()
+  });
+  // Initiate the viz
+  initViz();
+
+  function initViz(){
+       var placeholderDiv = document.getElementById("tableauViz");
+       var url = "https://public.tableau.com/views/IHC_prcnt_savings/ProjectswithCostSavings?:embed=y&:display_count=yes";
+       var options = {
+            hideTabs: true,
+            hideToolbar: true,
+            onFirstInteractive: function () {
+                  workbook = viz.getWorkbook();
+                  activeSheet = workbook.getActiveSheet();
+            }
+       };
+       viz = new tableauSoftware.Viz(placeholderDiv, url, options);
+      // Add event listener
+      // viz.addEventListener(tableauSoftware.TableauEventName.MARKS_SELECTION, onMarksSelection);
+  };
+});
--- ihc_data/ihc/views.py
+++ ihc_data/ihc/views.py
@@ -3,5 +3,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-
+    '''
+        Loads the index/home page
+    '''
     return render(request, 'index.html')
+
+
+def dashboard(request):
+    '''
+        Loads the page with all the Tableau things
+    '''
+    return render(request, 'dashboard.html')
